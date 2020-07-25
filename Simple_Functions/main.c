5022d786536e4c1ea203fb737ef69620a7e50cc6
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-07-25 19:22:05 +0000

    Test main

---
+++ Simple_Functions/main.c
@@ -0,0 +1,21 @@
+#include "Data_Structures.h"
+
+/**
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	int Array[] = {4,3,2,5,1,7,6,10,9,8,13,11,12};
+	int Size    = sizeof(Array) / sizeof(int);
+	int Index   = 0;
+	Element *head = NULL;
+
+	while (Index < Size)
+		Insert_Order_Nodo(&head, Array[Index++]);
+
+	print_Linked_ListR(head, 0);
+	return (0);
+}
