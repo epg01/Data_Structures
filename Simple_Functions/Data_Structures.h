a726e5387cbfffe3ef1a02162e781f6165a505ab
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-07-25 19:20:04 +0000

    Header file used in this folder

---
+++ Simple_Functions/Data_Structures.h
@@ -0,0 +1,17 @@
+#ifndef Data_Structures
+#define Data_Structures
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Element_
+{
+	int Data;
+	struct Element_ *Next;
+}Element;
+
+void print_Linked_ListR(Element *Pointer_Browse_List, int Index);
+void Insert_Order_Nodo(Element **Pointer_To_Pointer_Head, int Data);
+Element *Create_Nodo(int Data);
+
+#endif
