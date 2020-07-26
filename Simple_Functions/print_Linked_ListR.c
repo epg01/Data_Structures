c0fa4995282fce0410976dafccd7fa2972682cbd
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-07-26 06:19:07 +0000

    Analyze some trivial errors, such as when there is no linked list

--- Simple_Functions/print_Linked_ListR.c
+++ Simple_Functions/print_Linked_ListR.c
@@ -8,14 +8,19 @@
 
 void print_Linked_ListR(Element *Pointer_Browse_List, int Index)
 {
+	if (!(Pointer_Browse_List))
+	{
+		printf("No existe link list\n");
+		return ;
+	}
 	if (!(Pointer_Browse_List->Next))
 	{
-		printf("Elemento[%d] = Data -> [%d]\n", Index, Pointer_Browse_List->Data);
+		printf("Elemento[%d]\t =\t Data -> [%d]\n", Index, Pointer_Browse_List->Data);
 		return ;
 	}
 	else
 	{
-		printf("Elemento[%d] = Data -> [%d]\n", Index, Pointer_Browse_List->Data);
+		printf("Elemento[%d]\t =\t Data -> [%d]\n", Index, Pointer_Browse_List->Data);
 		print_Linked_ListR(Pointer_Browse_List->Next, ++Index);
 		return ;
 	}
