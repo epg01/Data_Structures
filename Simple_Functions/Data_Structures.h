8035186bea11508be94a522ff95895795ac452f7
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-07-26 07:24:07 +0000

    Another function was added, found in the file Remove_Nodo_Recursibely

--- Simple_Functions/Data_Structures.h
+++ Simple_Functions/Data_Structures.h
@@ -14,4 +14,6 @@ void print_Linked_ListR(Element *Pointer_Browse_List, int Index);
 void Insert_Order_Nodo(Element **Pointer_To_Pointer_Head, int Data);
 Element *Create_Nodo(int Data);
 void Delete_Nodo(int Data, Element **heap);
+int Remove_Node_R(int Data, Element **heap);
+
 #endif
