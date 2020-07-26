078c1f338e70d47c511e001abe4feaec3ef196f8
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-07-26 06:20:06 +0000

    Prototype of Delete_Nodo function added

--- Simple_Functions/Data_Structures.h
+++ Simple_Functions/Data_Structures.h
@@ -13,5 +13,5 @@ typedef struct Element_
 void print_Linked_ListR(Element *Pointer_Browse_List, int Index);
 void Insert_Order_Nodo(Element **Pointer_To_Pointer_Head, int Data);
 Element *Create_Nodo(int Data);
-
+void Delete_Nodo(int Data, Element **heap);
 #endif
